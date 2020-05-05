c1e6d494c4637acde757344e5fde6f56919da6cc
Michał Warchałowski <michalwarchalowski@gmail.com>
2020-05-05 19:40:58 +0200

    Add NpControllers and View

--- ParkyWeb/Startup.cs
+++ ParkyWeb/Startup.cs
@@ -8,6 +8,8 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using ParkyWeb.Repository;
+using ParkyWeb.Repository.IRepository;
 
 namespace ParkyWeb
 {
@@ -25,6 +27,8 @@ namespace ParkyWeb
         {
             services.AddControllersWithViews().AddRazorRuntimeCompilation();
             services.AddHttpClient();
+            services.AddScoped<ITrailRepository, TrailRepository>();
+            services.AddScoped<INationalParkRepository, NationalParkRepository>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
